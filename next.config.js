744d521d172439fe02151539e809ae83ef624088
Prajjwal Kapoor <prajjwal.kapoor.mail@gmail@con>
2022-09-26 01:20:58 +0530

    feat: version 0

--- next.config.js
+++ next.config.js
@@ -53,6 +53,10 @@ const securityHeaders = [
 ]
 
 module.exports = withBundleAnalyzer({
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   eslint: {
